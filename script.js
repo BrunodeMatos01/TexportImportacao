798ae8c686307f2febc47c06d96e672b5a3a17a4
Bruno Matos <brunodematos3@gmail.com>
2025-07-01 17:42:57 -0300

    Responsividade NAV

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-const btnMobile = document.getElementById('btn-mobile');
+const menu = document.getElementById('menu');
+  const menuIcon = document.querySelector('.menu-icon');
 
-function toggleMenu(event) {
-  if (event.type === 'touchstart') event.preventDefault();
-  const cabecalho = document.getElementById('cabecalho');
-  cabecalho.classList.toggle('nav-active');
-  const active = cabecalho.classList.contains('nav-active');
-  event.currentTarget.setAttribute('aria-expanded', active);
-  if (active) {
-    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-  } else {
-    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  function toggleMenu() {
+    menu.classList.toggle('show');
   }
-}
 
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+  // Fecha o menu ao clicar fora
+  document.addEventListener('click', function(event) {
+    const isClickInsideMenu = menu.contains(event.target);
+    const isClickOnIcon = menuIcon.contains(event.target);
+
+    // Se clicar fora do menu e fora do ícone
+    if (!isClickInsideMenu && !isClickOnIcon) {
+      menu.classList.remove('show');
+    }
+  });
+
+  // Fecha o menu ao clicar em um link
+  const links = menu.querySelectorAll('a');
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      menu.classList.remove('show');
+    });
+  });
