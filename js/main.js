912d8bf47adf5abe4498b8e5e727a7b1209144e3
Bruno Matos <brunodematos3@gmail.com>
2025-09-20 17:23:27 -0300

    ok

--- js/main.js
+++ js/main.js
@@ -253,7 +253,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 { marca: 'Lectra', modelo: 'Teclado Bolha NGC', nome: 'Teclado compatível com Máquina Lectra', img: '../img/imgProdutos/Teclado Bolha Lectra.png' },
             ],
             'Guia de Lâminas': [
-                { marca: 'Guia de Lâmina Lectra', modelo: 'Q50 IX6', nome: 'Guia de Lâminas compatível com Máquina Lectra', img: '../img/imgProdutos/Guia de Lamina Lectra' },
+                { marca: 'Lectra', modelo: 'Q50 IX6', nome: 'Guia de Lâminas compatível com Máquina Lectra', img: '../img/imgProdutos/Guia de Lamina Lectra.png' },
             ],
             'Bloco de Plastico': [
                 { marca: 'Lectra', modelo: 'IX6 IX9 MX9', nome: 'Bloco de Plastico compatível com Máquina Lectra', img: '../img/imgProdutos/Bloco Plastico Lectra.png' },
